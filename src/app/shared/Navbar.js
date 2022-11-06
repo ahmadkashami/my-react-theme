4749c1c8136944e5c383433a6bf1843d8061d9f4
aalkashami <aalkashami@easyapplyedu.com>
2022-11-06 10:10:15 +0300

    fix Navbar

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -7,15 +7,13 @@ class Navbar extends Component {
   toggleOffCanvas() {
     document.querySelector('.sidebar-offcanvas').classList.toggle('active');
   }
-  toggleRightSidebar() {
-    document.querySelector('.right-sidebar').classList.toggle('open');
-  }
+
   render () {
     return (
         <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
           <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <Link className="navbar-brand brand-logo" to="/"><img src={require('../../assets/images/logo.svg')} alt="logo" /></Link>
-            <Link className="navbar-brand brand-logo-mini" to="/"><img src={require('../../assets/images/logo-mini.svg')} alt="logo" /></Link>
+            <Link className="navbar-brand brand-logo" to="/"><img src={require("../../assets/images/logo-mini.svg")} alt="logo" /></Link>
+            <Link className="navbar-brand brand-logo-mini" to="/"><img src={require("../../assets/images/faces/face4.jpg")}  alt="logo" /></Link>
           </div>
           <div className="navbar-menu-wrapper d-flex align-items-stretch">
             <button className="navbar-toggler navbar-toggler align-self-center" type="button" onClick={ () => document.body.classList.toggle('sidebar-icon-only') }>
@@ -53,56 +51,6 @@ class Navbar extends Component {
                       <i className="mdi mdi-logout mr-2 text-primary"></i>
                       <Trans>Signout</Trans>
                     </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </li>
-              <li className="nav-item">
-                <Dropdown >
-                  <Dropdown.Toggle className="nav-link count-indicator">
-                    <i className="mdi mdi-email-outline"></i>
-                    <span className="count-symbol bg-warning"></span>
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu className="preview-list navbar-dropdown">
-                    <h6 className="p-3 mb-0"><Trans>Messages</Trans></h6>
-                    <div className="dropdown-divider"></div>
-                    <Dropdown.Item className="dropdown-item preview-item" onClick={evt =>evt.preventDefault()}>
-                      <div className="preview-thumbnail">
-                        <img src={require("../../assets/images/faces/face4.jpg")} alt="user" className="profile-pic"/>
-                      </div>
-                      <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                        <h6 className="preview-subject ellipsis mb-1 font-weight-normal"><Trans>Mark send you a message</Trans></h6>
-                        <p className="text-gray mb-0">
-                          1 <Trans>Minutes ago</Trans>
-                        </p>
-                      </div>
-                    </Dropdown.Item>
-                    <div className="dropdown-divider"></div>
-                    <Dropdown.Item className="dropdown-item preview-item" onClick={evt =>evt.preventDefault()}>
-                      <div className="preview-thumbnail">
-                        <img src={require("../../assets/images/faces/face2.jpg")} alt="user" className="profile-pic"/>
-                      </div>
-                      <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                        <h6 className="preview-subject ellipsis mb-1 font-weight-normal"><Trans>Cregh send you a message</Trans></h6>
-                        <p className="text-gray mb-0">
-                          15 <Trans>Minutes ago</Trans>
-                        </p>
-                      </div>
-                    </Dropdown.Item>
-                    <div className="dropdown-divider"></div>
-                    <Dropdown.Item className="dropdown-item preview-item" onClick={evt =>evt.preventDefault()}>
-                      <div className="preview-thumbnail">
-                        <img src={require("../../assets/images/faces/face3.jpg")} alt="user" className="profile-pic"/>
-                      </div>
-                      <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                        <h6 className="preview-subject ellipsis mb-1 font-weight-normal"><Trans>Profile picture updated</Trans></h6>
-                        <p className="text-gray mb-0">
-                          18 <Trans>Minutes ago</Trans>
-                        </p>
-                      </div>
-                    </Dropdown.Item>
-                    <div className="dropdown-divider"></div>
-                    <h6 className="p-3 mb-0 text-center cursor-pointer">4 <Trans>new messages</Trans></h6>
                   </Dropdown.Menu>
                 </Dropdown>
               </li>
@@ -161,16 +109,7 @@ class Navbar extends Component {
                   </Dropdown.Menu>
                 </Dropdown>
               </li>
-              <li className="nav-item nav-logout d-none d-lg-block">
-                <a className="nav-link" href="!#" onClick={event => event.preventDefault()}>
-                  <i className="mdi mdi-power"></i>
-                </a>
-              </li>
-              <li className="nav-item nav-settings d-none d-lg-block">
-                <button type="button" className="nav-link border-0" onClick={this.toggleRightSidebar} >
-                  <i className="mdi mdi-format-line-spacing"></i>
-                </button>
-              </li>
+
             </ul>
             <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" onClick={this.toggleOffCanvas}>
               <span className="mdi mdi-menu"></span>
